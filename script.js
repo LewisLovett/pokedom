6ae6f2aaca4bbd6e3053b0186baec46145b8abad
Lewis Lovett <llovett7575@gmail.com>
2022-10-04 14:59:22 +0100

    Added code a capitalize name

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ const renderCards = () => {
     let cardHTML = "";
     for(let pos in pokemonArray){
         let pokemon = pokemonArray[pos];
+        let pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
         cardHTML += '<div class="card">';
         cardHTML += `<img class="card__image" src =${pokemon.sprite}>`;
         cardHTML += '<div class="card__content">';
-        cardHTML += `<h2 class="card__heading">${pokemon.name}</h2>`;
-        cardHTML += `<p class="card__text">${pokemon.name}(#${pokemon.id}) is a ${pokemon.types[0]}`;
+        cardHTML += `<h2 class="card__heading">${pokemonName}</h2>`;
+        cardHTML += `<p class="card__text">${pokemonName}(#${pokemon.id}) is a ${pokemon.types[0]}`;
         if(pokemon.types.length > 1){
             for(let i = 1; i < pokemon.types.length; i++){
                 cardHTML += ` & ${pokemon.types[i]}`;
             }
         }
+        cardHTML += " type pokemon."
         cardHTML += "</div>";
         cardHTML += "</div>";
     }
