0221e94ddd847986f89ce7527b189e349ed4f5f1
Lewis Lovett <llovett7575@gmail.com>
2022-10-04 14:44:34 +0100

    Rendering for name, text and a loop for types

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ import pokemonArray from './data/pokemon.js';
 const renderCards = () => {
     const cardContainer = document.querySelector(".card-container");
     let cardHTML = "";
-    for(let pokemon in pokemonArray){
-        console.log(pokemonArray[pokemon].sprite);
-        
-        cardHTML += `<div class="card"><img class="card__image" src =${pokemonArray[pokemon].sprite}>`
+    for(let pos in pokemonArray){
+        let pokemon = pokemonArray[pos];
+        cardHTML += '<div class="card">';
+        cardHTML += `<img class="card__image" src =${pokemon.sprite}>`;
+        cardHTML += '<div class="card__content">';
+        cardHTML += `<h2 class="card__heading">${pokemon.name}</h2>`;
+        cardHTML += `<p class="card__text">${pokemon.name}(#${pokemon.id}) is a ${pokemon.types[0]}`;
+        if(pokemon.types.length > 1){
+            for(let i = 1; i < pokemon.types.length; i++){
+                cardHTML += ` & ${pokemon.types[i]}`;
+            }
+        }
+        cardHTML += "</div>";
         cardHTML += "</div>";
     }
     cardContainer.innerHTML += cardHTML;
